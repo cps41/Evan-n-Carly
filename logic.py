0cbfd5d49862bbb47a866c7c2935d58f46a30109
Carly <cps41@pitt.edu>
2022-03-19 20:41:18 -0400

    test

---
+++ logic.py
@@ -0,0 +1,17 @@
+import keyboard
+
+class Timer_Typer:
+    def __init__(self):
+        pass
+    def event_loop(self):
+        while True:
+            if keyboard.read_key() == "enter":
+                print('Pressed Enter')
+                break
+
+def main():
+    t = Timer_Typer()
+    t.event_loop()
+
+if __name__ == '__main__':
+    main()
